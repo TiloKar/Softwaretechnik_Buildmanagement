849c6f7fef8c7dd6e0b0a288a0b2dc6f2f299985
tilo <tilo.karczewski@gmx.de>
2021-06-18 08:55:17 +0200

    init

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"HonorarRechner"},{"p":"<Unnamed>","l":"HonorarRechnerJUnit3Test"},{"p":"<Unnamed>","l":"TextAnalyze"}]
